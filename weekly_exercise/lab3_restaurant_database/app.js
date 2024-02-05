19d91b0df41a2da96118d3ff278a2e2c79336a67
Matt-239 <105237637+Matt-239@users.noreply.github.com>
2024-02-04 23:11:19 -0500

    lab3

--- weekly_exercise/lab3_restaurant_database/app.js
+++ weekly_exercise/lab3_restaurant_database/app.js
@@ -5,8 +5,11 @@ const Restaurant = require('./restaurantModel');
 const app = express();
 const PORT = 3000;
 
-mongoose.connect('mongodb://localhost:27017/restaurantDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const mongodbURI = 'mongodb+srv://mprice239:FbaMOio4rVkizpRJ@cluster0.cbbhvr6.mongodb.net/?retryWrites=true&w=majority';
 
+mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { console.log('Connected to MongoDB') }).catch(err => console.log(err));
+
+// GET all restaurants
 app.get('/restaurants', async (req, res) => {
     try {
         const restaurants = await Restaurant.find();
@@ -16,42 +19,44 @@ app.get('/restaurants', async (req, res) => {
     }
 });
 
+// GET all restaurants of type cuisine
+app.get('/restaurants/cuisine/:style', async (req, res) => {
+    try {
+        const style = req.params.style;
+        const restaurants = await Restaurant.find({cuisines: style});
+        res.json(restaurants);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// GET all restaurants sorted by name
 app.get('/restaurants', async (req, res) => {
     const sortBy = req.query.sortBy || 'ASC';
     try {
         const restaurants = await Restaurant.find()
-            .select('id cuisines name city restaurant_id')
-            .sort({ restaurant_id: sortBy === 'ASC' ? 1 : -1});
+        .select('id cuisines name city restaurant_id')
+        .sort({restaurant_id: sortBy === 'ASC' ? 1 : -1});
         res.json(restaurants);
-    }
-    catch (error) {
+    } catch (error) {
         res.status(500).send(error.message);
     }
 });
 
+// get restaurant by condition
 app.get('/restaurants/:cuisine', async (req, res) => {
-    const cuisine = req.params.cuisine;
-    try {
-        const restaurants = await Restaurant.find({
-            cuisines: cuisine,
-            city: { $ne: 'Brooklyn' }
-        })
-            .select('cuisines name city -id')
-            .sort({ name: 1 });
-        res.json(restaurants);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.get('/restaurants/cuisine/:cuisine', async (req, res) => {
-    const cuisine = req.params.cuisine;
-    try {
-        const restaurants = await Restaurant.find({cuisines: cuisine});
-        res.json(restaurants);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  const cuisine = req.params.cuisine;
+  try {
+    const restaurants = await Restaurant.find({
+      cuisines: cuisine,
+      city: { $ne: 'Brooklyn' }
+    })
+      .select('cuisines name city -id')
+      .sort({ name: 1 });
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 
--- weekly_exercise/lab3_restaurant_database/restaurantModel.js
+++ weekly_exercise/lab3_restaurant_database/restaurantModel.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 
 const restaurantSchema = new mongoose.Schema({
-    id: String,
+    address: {
+        building: String,
+        street: String,
+        zipcode: String
+    },
     cuisines: String,
     name: String,
     city: String,
-    restaurant_id: String
+    restaurant_id: Number
 });
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
-
-module.exports = Restaurant;
+module.exports = mongoose.model('Restaurant', restaurantSchema);
