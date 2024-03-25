36eca05fd0406e2aa6e4a939816a182659e9b57e
Matt-239 <105237637+Matt-239@users.noreply.github.com>
2024-03-24 20:11:49 -0400

    adds tour of heroes angular project

---
+++ angular/tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,22 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr.-Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr.-IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+  { id: 21, name: 'Spider-Man' },
+  { id: 22, name: 'Batman' },
+  { id: 23, name: 'Superman' },
+  { id: 24, name: 'Iron-Man' },
+  { id: 25, name: 'Thor' },
+  { id: 26, name: 'Hulk' },
+  { id: 27, name: 'Captain-America' },
+  { id: 28, name: 'Black-Widow' },
+  { id: 29, name: 'Hawkeye' },
+];
